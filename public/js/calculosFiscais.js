85ce03ebd8b6daee2d43cd50c44648d8a2ad3b62
Gabriel de Jesus <dejesus1gabriel@gmail.com>
2026-04-14 20:17:47 +0000

    alteração de calculo fiscal para isenção de imposto no codin/rj

--- public/js/calculosFiscais.js
+++ public/js/calculosFiscais.js
@@ -12,29 +12,45 @@ function calcularPrecificacao(dados) {
     const difalPct = (Number(dados.difal) || 0) / 100;
     const freteML = Number(dados.frete_ml) || 0;
     const stPct = dados.flag_simulacao_st ? 0.0 : ((Number(dados.st) || 0) / 100);
+    const codin = dados.flag_simulacao_st;
 
-    const valorICMSEnt = custo * icmsEntPct;
     const valorIPI = custo * ipiPct;
     const valorST = custo * stPct;
-    const basePisCofinsEnt = custo - valorICMSEnt + valorIPI;
-    const creditoPisCofins = basePisCofinsEnt * CONSTANTES_FISCAIS.PIS_COFINS;
-    const valorLiquido = custo - valorICMSEnt - creditoPisCofins + valorIPI + valorST;
 
     const calcularCenario = (precoVenda, taxaPct) => {
         const preco = Number(precoVenda) || 0;
+
+        const valorICMSEnt = codin ? preco * icmsEntPct : custo * icmsEntPct;
+
+        const basePisCofinsEnt = custo - valorICMSEnt + valorIPI;
+        const creditoPisCofins = basePisCofinsEnt * CONSTANTES_FISCAIS.PIS_COFINS;
+
+        const valorLiquido = custo - valorICMSEnt - creditoPisCofins + valorIPI + valorST;
+
         const taxaML = preco * taxaPct;
         const valorICMSSai = preco * icmsSaiPct;
         const basePisCofinsSai = preco - valorICMSSai;
         const debitoPisCofins = basePisCofinsSai * CONSTANTES_FISCAIS.PIS_COFINS;
         const valorDifal = preco * difalPct;
+
         const custoTotal = valorLiquido + freteML + taxaML + debitoPisCofins + valorICMSSai + valorDifal;
         const margem = preco > 0 ? ((preco - custoTotal) / preco) * 100 : 0;
-        return { taxaML, valorICMSSai, debitoPisCofins, valorDifal, custoTotal, margem };
+
+        return { taxaML, valorICMSSai, debitoPisCofins, valorDifal, custoTotal, margem, valorICMSEnt, valorIPI, valorST, creditoPisCofins, valorLiquido };
     };
 
+    const classico = calcularCenario(dados.preco_classico, CONSTANTES_FISCAIS.TAXA_CLASSICO);
+    const premium = calcularCenario(dados.preco_premium, CONSTANTES_FISCAIS.TAXA_PREMIUM);
+
     return {
-        custoBase: { valorICMSEnt, valorIPI, valorST, creditoPisCofins, valorLiquido },
-        classico: calcularCenario(dados.preco_classico, CONSTANTES_FISCAIS.TAXA_CLASSICO),
-        premium: calcularCenario(dados.preco_premium, CONSTANTES_FISCAIS.TAXA_PREMIUM)
+        custoBase: {
+            valorICMSEnt: classico.valorICMSEnt,
+            valorIPI: classico.valorIPI,
+            valorST: classico.valorST,
+            creditoPisCofins: classico.creditoPisCofins,
+            valorLiquido: classico.valorLiquido
+        },
+        classico,
+        premium
     };
 }
